acd0649eeb1984e64b33bfa9fb9c8f9d4e5f5a06
wizjatelo <wisdommarkanaynge@gmail.com>
2025-07-29 16:54:18 +0300

    Clean up: ignore node_modules

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomerDashboard from './CustomerDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerDashboard />
+    </div>
+  );
+}
+
+export default App;
